5d44c630f37b6479fda694e4835fd7e09b2266f6
Jesse Scott <xalmia@gmail.com>
2018-05-01 16:21:14 -0600

    Added files for index, generated php framework ad testing.

--- web/index.php
+++ web/index.php
@@ -1,5 +1,18 @@
+<<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Jesse Scott's CS313 Landing Page</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="indexStyle.css" />
+  <script src="indexScripts.js"></script>
+</head>
+<body>
+  <h1>Hello World</h1>
 <?php
-
-  phpinfo();
-
+  $var = "<p>:V</p><br/>";
+  for($i=0;i<10;$i++){
+    echo "$var";
+  }
 ?>
+</body>
+</html>
