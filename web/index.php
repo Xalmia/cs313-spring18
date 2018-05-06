46773d20478f436b57f4ca3c9001aeb42d00725c
Jesse Scott <xalmia@gmail.com>
2018-05-05 18:26:28 -0600

    Assignment 1 date/time and assignment test

--- web/index.php
+++ web/index.php
@@ -7,25 +7,27 @@
   <script src="indexScripts.js"></script>
 </head>
 <body>
-<div id="header" class="header">
+<div id="header" class="header, bordered">
   <h1>Jesse Scott's Homepage</h1>
   <br/>
   <?php 
     date_default_timezone_set("America/Boise");
+    $currentTimezone = date_default_timezone_get();
     // Variable that holds the date to be displayed.
-    $current_date = date('d/m/Y == h:i:s');
-    echo "<h3>$current_date $currentTimezone</h3>"
+    $current_date = date('d/m/Y == h:i:s $timezone');
+    echo "<h3>$current_date $currentTimezone</h3>";
   ?>
 </div>
-<div id="assignments" class="sidebar">
+<div id="assignments" class="assignmentBar, bordered">
+  <a href="aboutMe.php">About Me</a>
 <?php
   for($i = 0; $i < 10; $i++){
     echo "<a href='/assignments/assignment$i.php'>Assignment $i</a> <br/>";
   }
 ?>
 </div>
-<div id="about">
-
+<div id="about" class="">
+  <p>Hi there. My name is Jesse Scott. I am a Software Engineering major</p>
 </div>
 </body>
 </html>
