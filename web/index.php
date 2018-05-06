1b9900cf9b402dedb45940e8ff5e72fa8ff69977
Jesse Scott <xalmia@gmail.com>
2018-05-05 18:30:08 -0600

    Assignment 1 date/time and assignment test

--- web/index.php
+++ web/index.php
@@ -12,14 +12,16 @@
   <br/>
   <?php 
     date_default_timezone_set("America/Boise");
-    $currentTimezone = date_default_timezone_get();
     // Variable that holds the date to be displayed.
-    $current_date = date('d/m/Y == h:i:s $timezone');
+    $currentTimezone = date_default_timezone_get();
+    
+    $current_date = date('d/m/Y == h:i:s');
     echo "<h3>$current_date $currentTimezone</h3>";
   ?>
 </div>
 <div id="assignments" class="assignmentBar, bordered">
   <a href="aboutMe.php">About Me</a>
+  <br/>
 <?php
   for($i = 0; $i < 10; $i++){
     echo "<a href='/assignments/assignment$i.php'>Assignment $i</a> <br/>";
