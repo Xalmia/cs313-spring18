1d40a93fcc40d605a62e06cfdc280510977639ae
Jesse Scott <xalmia@gmail.com>
2018-05-10 15:41:47 -0600

    Add changes to about

--- web/aboutMe.php
+++ web/aboutMe.php
@@ -6,8 +6,13 @@
     <link rel="stylesheet" type="text/css" media="screen" href="indexStyle.css" />
 </head>
 <body>
-    <div id="about" class="Header">
-        <h2>Hi there. My name is Jesse Scott. I am a Software Engineering major</h2>
+    <div id="about" class="bordered header">
+        <h2>Hi there. My name is Jesse Scott.</h2>
+        <p id="classDescription" class="descriptive-text">
+            I am a Software Engineering major, with about two semesters left before I graduate.
+            I enjoy puppies and cooking.
+        </p>
     </div>
+    <a id="backLink" class="back-link" href="/index.php"> <-Back- </a>
 </body>
 </html>
