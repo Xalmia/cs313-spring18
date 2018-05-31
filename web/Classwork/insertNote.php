6cde6a8451ed043cd3a499507d21c0b3e4bd494d
Jesse Scott <xalmia@gmail.com>
2018-05-31 16:47:36 -0600

    finished pager interactions, inserted redirect

--- web/Classwork/insertNote.php
+++ web/Classwork/insertNote.php
@@ -4,8 +4,22 @@
     $date = htmlspecialchars($_POST["date"]);
     $content = htmlspecialchars($_POST["content"]);
 
-    echo "Course Id: $course_id";
-    echo "Date: $date";
-    echo "content: $content";
-    
+    //echo "Course Id: $course_id";
+    //echo "Date: $date";
+    //echo "content: $content";
+    require("dbConnect.php");
+
+    $db = get_db();
+
+    $query = "INSERT INTO note (course_id, content, date)
+        VALUES (:course_id, :content, :date)";
+    $statement = $db->prepare($query);
+    $statement.bindValue(":course_id", $course_id, PDO::PARAM_INT);
+    $statement.bindValue(":content", $content, PDO::PARAM_STR);
+    $statement.bindValue(":date", $date, PDO::PARAM_STR);
+
+    $statement->execute();
+
+    header("location: courseDetails.php?course_id=$course_id");
+    die();
 ?>
