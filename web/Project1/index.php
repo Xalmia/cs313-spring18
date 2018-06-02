8b1232a3338068606bc4440883d055e6549f25d5
Jesse Scott <xalmia@gmail.com>
2018-06-02 17:49:40 -0600

    Troubleshooting display errors

--- web/Project1/index.php
+++ web/Project1/index.php
@@ -34,7 +34,7 @@
         <ul class="journal-list"> 
             <?php //foreach section, print a thing
                 $sectionName = $section["section_title"];
-                objdump($section);
+                var_dump($section);
                 echo "<li>$sectionName</li>"
             ?>
         </ul>
