0989dc8176d3db7fa2f84a32925907330bcb696a
Jesse Scott <xalmia@gmail.com>
2018-06-05 16:29:16 -0600

    Added PDO::params

--- web/Classwork/Week6/registerUser.php
+++ web/Classwork/Week6/registerUser.php
@@ -9,8 +9,8 @@
     $query = "INSERT INTO user_table (username, user_password) VALUES (':userN', ':passW');";
 
     $statement = $db->prepare($query);
-    $statement->bindValue(':userN', $uName);
-    $statement->bindValue(':passW', $pWord);
+    $statement->bindValue(':userN', $uName, PDO::PARAM_STR);
+    $statement->bindValue(':passW', $pWord, PDO::PARAM_STR);
     $statement->execute();
 
     header('Location: index.php');
